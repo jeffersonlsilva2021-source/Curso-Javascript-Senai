d85970f7e1213c754eb69cd763f3577944671a29
Jefferson Silva <Jefferson.lsilva2021@gmail.com>
2026-05-22 19:30:38 -0300

    criando funções

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("Executado pelo arquivo index.js")
+
+function somarNumeros(x, y) {
+    console.log("Aqui é feito a soma dos numeros")
+    let soma = x + y
+    console.log(soma)
+}
+
+somarNumeros(5, 8) // 1 chamada
+somarNumeros(10, 10) // 2 chamada
+somarNumeros(9, 5) // 3 chamada
+somarNumeros(7, 3) // 4 chamada
+
